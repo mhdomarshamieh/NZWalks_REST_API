c7e55ea5f80219aff341c87a17f24bd04808d2d5
MHD-OMAR-SHAMIEH <103579045+MHD-OMAR-SHAMIEH@users.noreply.github.com>
2024-07-22 23:20:35 +0300

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = {"JOHN", "JASON", "NIDAL"};
+
+            return Ok(students);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,22 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+
+        public string? WalkImageUrl { get; set; }
+
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId {  get; set; }
+
+        //Navigation properties
+        // These models basically tell the entity framework that
+        // these Ids above are foreign keys for walk table
+        // in order to create the neccesary relationships 
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
