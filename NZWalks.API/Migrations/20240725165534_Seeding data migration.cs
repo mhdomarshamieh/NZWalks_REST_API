f3a9ef59acee1d3eb7c83e3e422539d32cb64794
MHD-OMAR-SHAMIEH <103579045+MHD-OMAR-SHAMIEH@users.noreply.github.com>
2024-07-25 21:38:44 +0300

    implementing Walk controller crud operation with using the Navigation Properties

---
+++ NZWalks.API/Migrations/20240725165534_Seeding data migration.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seedingdatamigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("4235c5d7-aef7-4bc0-a1d2-df992cce60f9"), "Easy" },
+                    { new Guid("55d64462-3f48-442c-9ee9-8840b18766f2"), "Hard" },
+                    { new Guid("e56830bf-e8f2-4f80-82af-c6295796104e"), "Medium" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("14ceba71-4b51-4777-9b17-46602cf66153"), "BOP", "Bay Of Plenty", null },
+                    { new Guid("6884f7d7-ad1f-4101-8df3-7a6fa7387d81"), "NTL", "Northland", null },
+                    { new Guid("906cb139-415a-4bbb-a174-1a1faf9fb1f6"), "NSN", "Nelson", "https://images.pexels.com/photos/13918194/pexels-photo-13918194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+                    { new Guid("cfa06ed2-bf65-4b65-93ed-c9d286ddb0de"), "WGN", "Wellington", "https://images.pexels.com/photos/4350631/pexels-photo-4350631.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+                    { new Guid("f077a22e-4248-4bf6-b564-c7cf4e250263"), "STL", "Southland", null },
+                    { new Guid("f7248fc3-2585-4efb-8d1d-1c555f4087f6"), "AKL", "Auckland", "https://images.pexels.com/photos/5169056/pexels-photo-5169056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("4235c5d7-aef7-4bc0-a1d2-df992cce60f9"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("55d64462-3f48-442c-9ee9-8840b18766f2"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("e56830bf-e8f2-4f80-82af-c6295796104e"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("14ceba71-4b51-4777-9b17-46602cf66153"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("6884f7d7-ad1f-4101-8df3-7a6fa7387d81"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("906cb139-415a-4bbb-a174-1a1faf9fb1f6"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("cfa06ed2-bf65-4b65-93ed-c9d286ddb0de"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("f077a22e-4248-4bf6-b564-c7cf4e250263"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("f7248fc3-2585-4efb-8d1d-1c555f4087f6"));
+        }
+    }
+}
